dd939ef263204b89d581dc915e7a517b6b7464fe
jltaglia <jltaglia@gmail.com>
2022-07-17 13:36:59 -0300

    Rev 17-7-2022 13:36

--- calcula_licencia.py
+++ calcula_licencia.py
@@ -22,8 +22,10 @@ def calc_lic_en_curso(id_empleado, estado, *fechas):
     CALCULA LA LICENCIA DE UN EMPLEADO
 
     id_empleado: ID DEL EMPLEADO
-    estado: 'alta' = al empleado se le está gestionando el alta en la empresa
-            'norm' = al empleado se le está calculando la licencia solicitada
+         estado: 'alta' = al empleado se le está gestionando el alta en la empresa
+                 'norm' = al empleado se le está calculando la licencia solicitada
+	     fechas: lista de fechas de inicio y fin de la licencia
+
     '''
     licen_curso = 0
     if estado == 'alta':
@@ -37,69 +39,54 @@ def calc_lic_en_curso(id_empleado, estado, *fechas):
     else:
         saldo_lic = 0
         licen_curso = 0
+        fecha_inicio, fecha_fin = fechas
 
-
-        finie, ffine = fechas
         print(DIA_ACT, MES_ACT, ANIO_ACT)
-        print(finie, ffine)
+        print(fecha_inicio, fecha_fin)
 
-
-        fecha1 = dt.datetime.strptime('01 06 2005', '%d %m %Y').date() 
-        fecha2 = dt.date(2005, 6, 1)
-        fecha3 = stod(finie)
-
-        print(fecha1, fecha2, fecha3)
-
-        date_1 = dt.datetime.strptime(finie, "%d/%m/%Y")
-        end_date = date_1 + dt.timedelta(days=10)
-
-        print(end_date.date())
-
-
-
-            # @ 8,4 say "Licencia a gozar desde el          hasta el "
-			# 	@ 8,30 get finie
-			# 	@ 8,48 get ffine valid ffine >= finie
-        # if w(ffine + 1) = 1 # LICIENCIA COMIENZA EN DIA DOMINGO
-    # 			ddreg := ffine + 2
+		# Licencia a gozar desde el fecha_inicio hasta el fecha_fin
+		# fecha_inicio
+		# fecha_fin valid fecha_fin >= fecha_inicio
+        # if dt.datetime.weekday(fecha_fin + 1) == 1: # LICIENCIA COMIENZA EN DIA DOMINGO
+     	# 		ddreg = fecha_fin + dt.timedelta(days=2)
             
-    # 		case (ffine + 1) = ctod("25/12/" + str(year(ffine),4)) // NAVIDAD
-    # 			ddreg := ffine + 2
+    # 		case (fecha_fin + 1) == ctod("25/12/" + str(year(fecha_fin),4)) // NAVIDAD
+    # 			ddreg = fecha_fin + 2
             
-    # 		case (ffine + 1) = ctod("01/01/" + str(year(ffine),4)) // A�O NUEVO
-    # 			ddreg := ffine + 2
+    # 		case (fecha_fin + 1) == ctod("01/01/" + str(year(fecha_fin),4)) // A�O NUEVO
+    # 			ddreg = fecha_fin + 2
             
-    # 		case (ffine + 1) = ctod("01/05/" + str(year(ffine),4)) // DIA DEL TRABAJO
-    # 			ddreg := ffine + 2
+    # 		case (fecha_fin + 1) == ctod("01/05/" + str(year(fecha_fin),4)) // DIA DEL TRABAJO
+    # 			ddreg = fecha_fin + 2
             
     # 		otherwise
-    # 			ddreg := ffine + 1
+    # 			ddreg = fecha_fin + 1
     # 	endcase
-			# 	@ 10,4 say "Fecha estimada de regreso :" get ddreg valid ddreg > ffine
+			# 	@ 10,4 say "Fecha estimada de regreso :" get ddreg valid ddreg > fecha_fin
 			# 	@ 12,4 say "La licencia ingresada..."
 			# 	@ 13,4 say "� Afecta a las vacaciones disponibles ? [SI=Y / NO=N]" get afecta picture "Y"
 			# 	set cursor on
 			# 	read
 			# 	set cursor off
-			# 	son := alert ("� Confirma los datos ingresados ?", {" SI "," NO "})
-			# 	if son = 2
+			# 	son = alert ("� Confirma los datos ingresados ?", {" SI "," NO "})
+			# 	if son == 2
 			# 		loop // 1
 			# 	endif
 			# 	//
 			# 	// ACTUALIZACION DE ARCHIVOS DE EMPLEADOS
 			# 	//
-			# 	archivo := "LEG_" + padl(alltrim(str(vcemp)),3,"0")
-			# 	archivoc := dirb6 + archivo + ".dbf"
+			# 	archivo = "LEG_" + padl(alltrim(str(vcemp)),3,"0")
+			# 	archivoc = dirb6 + archivo + ".dbf"
 			# 	//
 			# 	use (archivoc);
 			# 		index (dirb6 + archivo + ".ntx");
 			# 		alias em new
 			# 	select em
 			# 	em -> (dbappend())
-			# 	replace em -> desde with finie
-			# 	replace em -> hasta with ffine
+			# 	replace em -> desde with fecha_inicio
+			# 	replace em -> hasta with fecha_fin
 			# 	replace em -> motivo with evento
-			# 	replace em -> cantidad with (ffine - finie) + 1
+			# 	replace em -> cantidad with (fecha_fin - fecha_inicio) + 1
 			# 	select ps
 			# 	replace ps -> fe_regreso with ddreg
 			# 	if afecta
@@ -107,17 +94,17 @@ def calc_lic_en_curso(id_empleado, estado, *fechas):
 			# 	// SI LA LICENCIA INGRESADA AFECTA
 			# 	// AL SALDO ACTUAL DE DIAS DE VACACIONES
 			# 	//
-			# 		nvo_saldo := ps -> saldo_lic - em -> cantidad
+			# 		nvo_saldo = ps -> saldo_lic - em -> cantidad
 			# 		if nvo_saldo < 0
-			# 			resto := abs(nvo_saldo)
-			# 			vie_licc := ctod(;
+			# 			resto = abs(nvo_saldo)
+			# 			vie_licc = ctod(;
 			# 					str(day(ps -> f_ing))+ "/" +;
 			# 					str(month(ps -> f_ing))+ "/" +;
 			# 					str(ps -> lic_curso);
 			# 					)
 						
-			# 			vie_dlic := dias_d_lic(ps -> f_ing , vie_licc)
-			# 			vlic_curso := ctod(;
+			# 			vie_dlic = dias_d_lic(ps -> f_ing , vie_licc)
+			# 			vlic_curso = ctod(;
 			# 					str(day(ps -> f_ing))+ "/" +;
 			# 					str(month(ps -> f_ing))+ "/" +;
 			# 					str(ps -> lic_curso + 1);
@@ -125,7 +112,7 @@ def calc_lic_en_curso(id_empleado, estado, *fechas):
 			# 			// 
 			# 			// CALCULO DE LOS DIAS DE LICENCIA
 			# 			//
-			# 			dias_lic := dias_d_lic(ps -> f_ing , vlic_curso)
+			# 			dias_lic = dias_d_lic(ps -> f_ing , vlic_curso)
 			# 			//
 			# 			//
 			# 			//
@@ -133,18 +120,18 @@ def calc_lic_en_curso(id_empleado, estado, *fechas):
 			# 			replace ps -> lic_curso with year(vlic_curso)
 			# 		else
 			# 			if nvo_saldo <> 0
-			# 				vlic_curso := ctod(;
+			# 				vlic_curso = ctod(;
 			# 					str(day(ps -> f_ing))+ "/" +;
 			# 					str(month(ps -> f_ing))+ "/" +;
 			# 					str(ps -> lic_curso))
 			# 			else
-			# 				vie_licc := ctod(;
+			# 				vie_licc = ctod(;
 			# 						str(day(ps -> f_ing))+ "/" +;
 			# 						str(month(ps -> f_ing))+ "/" +;
 			# 						str(ps -> lic_curso);
 			# 						)
-			# 				vie_dlic := dias_d_lic(ps -> f_ing , vie_licc)
-			# 				vlic_curso := ctod(;
+			# 				vie_dlic = dias_d_lic(ps -> f_ing , vie_licc)
+			# 				vlic_curso = ctod(;
 			# 					str(day(ps -> f_ing))+ "/" +;
 			# 					str(month(ps -> f_ing))+ "/" +;
 			# 					str(ps -> lic_curso + 1);
@@ -153,15 +140,15 @@ def calc_lic_en_curso(id_empleado, estado, *fechas):
 			# 			// 
 			# 			// CALCULO DE LOS DIAS DE LICENCIA
 			# 			//
-			# 			dias_lic := dias_d_lic(ps -> f_ing , vlic_curso)
+			# 			dias_lic = dias_d_lic(ps -> f_ing , vlic_curso)
 			# 			//
 			# 			//
 			# 			//
-			# 			if nvo_saldo = 0
+			# 			if nvo_saldo == 0
 			# 				replace ps -> lic_curso with ps -> lic_curso + 1
 			# 				replace ps -> saldo_lic with dias_lic
 			# 			else
-			# 				diasytom := dias_lic - ps -> saldo_lic
+			# 				diasytom = dias_lic - ps -> saldo_lic
 			# 				replace ps -> lic_curso with year(vlic_curso)
 			# 				replace ps -> saldo_lic with nvo_saldo
 			# 			endif
